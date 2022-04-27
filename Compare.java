8d63c46ebbcc1643e31f037c3b87942ce77dfc65
srushti3653 <103207279+srushti3653@users.noreply.github.com>
2022-04-27 18:25:39 +0530

    Add files via upload

---
+++ Compare.java
@@ -0,0 +1,15 @@
+package if_else;
+
+public class Compare {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 89;
+        if(a==b){
+            System.out.println("a is equals to b");
+        }
+        else{
+            System.out.println("a is not equals to b");
+        }
+    }   
+    
+}
